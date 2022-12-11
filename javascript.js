ff84be51cf26308cadb3dc53c8a1e1ad4e8357ab
Arijana <arijana.maatta@metropolia.fi>
2022-12-11 15:59:37 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,56 @@
+const search = document.querySelector("form");
+const showCm = document.querySelector("#shows");
+const dialoog = document.querySelector("dialog");
+const closeM = document.querySelector("#closeModel");
+const titleE = document.querySelector("#modalTitle");
+
+closeM.addEventListener("click", () => dialoog.close());
+
+search.addEventListener("submit", async function(submitEvent) {
+  submitEvent.preventDefault();
+  const formData = new FormData(this);
+  const search = new URLSearchParams(formData).toString();
+  const fetchData = await fetch(this.action + search);
+  const jsonData = await fetchData.json();
+  
+  if(jsonData.length) showCm.innerHTML = "";
+  else showCm.innerHTML = "<p>No results</p>";
+  jsonData.forEach(result => {
+    const {show} = result;
+    const containerE = document.createElement("div");
+    containerE.classList.add("show");
+    const nameEl = document.createElement("h2");
+    nameEl.textContent = show.name;
+    console.log(nameEl)
+  
+    const detailsEl = document.createElement("a");
+    detailsEl.href = show.url;
+    detailsEl.target = "details";
+    detailsEl.textContent = "More details";
+
+    detailsEl.addEventListener("click", () => {
+      dialoog.showModal();
+      modalTitleElem.textContent = show.name;
+    });
+  
+    const img = document.createElement("img");
+    img.src = show.image?.medium ?? "./img/no_img.png";
+    
+    const genress = document.createElement("div");
+    genress.innerHTML += "<h2>Genres</h2>"
+    genress.classList.add("genres");
+    show.genres.forEach(genre => {
+      const genreElem = document.createElement("p");
+      genreElem.textContent = genre;
+      genreElem.classList.add("genre");
+      genress.appendChild(genreElem);
+    });
+  
+    const summaryElem = document.createElement("div");
+    summaryElem.classList.add("summary");
+    summaryElem.innerHTML = show.summary;
+    containerE.append(nameEl, img, summaryElem, detailsEl);
+    if(show.genres?.length) img.after(genress)
+    showCm.append(containerE);
+  });
+});
